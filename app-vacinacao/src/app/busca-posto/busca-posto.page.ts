cc5eb3e0ffebc8848034d699a3b7f850f1190c81
nathaliadutra <nathaliad.oliveira@hotmail.com>
2021-09-28 22:54:09 -0300

    Padronização dos estilos das páginas e criação da nova rota de busca de postos

---
+++ app-vacinacao/src/app/busca-posto/busca-posto.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busca-posto',
+  templateUrl: './busca-posto.page.html',
+  styleUrls: ['./busca-posto.page.scss'],
+})
+export class BuscaPostoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
