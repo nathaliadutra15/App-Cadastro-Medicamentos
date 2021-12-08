10d65735a83b781e1fa25a3a72c3a28e102a1144
nathaliadutra <nathaliad.oliveira@hotmail.com>
2021-12-07 21:45:29 -0300

    API REST dos Medicamentos

--- app-vacinacao/src/app/meus-alarmes/meus-alarmes.page.ts
+++ app-vacinacao/src/app/meus-alarmes/meus-alarmes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-meus-alarmes',
@@ -12,10 +13,25 @@ export class MeusAlarmesPage {
 
   public api;
   public listaRemedios = [];
+  public idRemedio;
+  public nomeMedicamento: string;
+  public intervaloH: number;
+  public switch = false;
+  public quantidade_pilulas = 0;
 
-  constructor(public httpClient: HttpClient) {
+
+  constructor(public httpClient: HttpClient, public alertController:AlertController) {
     this.listarRemedios();
-   }
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 
   async listarRemedios() {
     //Fazendo uma requisição do body da nossa API criada
@@ -31,10 +47,108 @@ export class MeusAlarmesPage {
     })
   }
 
-  async deletarRemedio(id:number){
+  async buscarRemedio(id: number) {
+    //Fazendo uma requisição do body da nossa API criada
+    this.httpClient.get(`http://localhost:4000/medicamentos/${id}`).subscribe((response) => {
+      //Passando o body (a api) para uma variável
+      this.api = response;
+      //Passando para a variável, a quantidade de elementos dentro da API
+      
+    });
+    return String(this.api[0].nomeMedicamento);
+  }
+  
 
-    this.httpClient.delete('http://localhost:4000/removerMedicamento/:id')
 
+  async deletarRemedio(id: number) {
+    this.httpClient.delete(`http://localhost:4000/removerMedicamento/${id}`).subscribe((response) => {
+      alert(response);
+    },
+    erro => {
+      console.log(erro);
+    });
+
+    this.listarRemedios();
+  }
+
+  atualizarRemedio(id: number) {
+
+    let remedio = {
+      nomeMedicamento: this.nomeMedicamento.toUpperCase(),
+      intervaloH: this.intervaloH,
+      qntdPilula: this.quantidade_pilulas,
+      liquido: this.switch
+    };
+
+
+    this.httpClient.put(`http://localhost:4000/atualizarMedicamento/${id}`, remedio).subscribe(
+      resultado => {
+        alert(resultado);
+      },
+      erro => {
+        console.log(erro);
+      }
+    );
+  }
+
+
+  async entradaAtualizacao(id:number) {
+    const alert = await this.alertController.create({
+      cssClass: 'alertaAtualizar',
+      header: 'Editar Alarme',
+      inputs: [
+        {
+          name: 'nomeMedicamento',
+          type: 'text',
+          placeholder:'Nome do medicamento'
+        },
+        {
+          name: 'intervaloH',
+          type: 'number',
+          placeholder: 'Intervalo/H entre as doses',
+        },
+        {
+          name: 'qntdPilula',
+          type: 'number',
+          placeholder: 'Quantidade de pílulas'
+        },
+        {
+          name: 'radio1',
+          type: 'checkbox',
+          label: 'Líquido',
+          placeholder: 'Este remédio é líquido?',
+          handler: () => {
+            console.log('Radio 1 selected');
+          },
+          checked: false,
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Salvar',
+          handler: salvar => {
+              this.nomeMedicamento = salvar.nomeMedicamento;
+              this.intervaloH = salvar.intervaloH;
+
+              if (salvar.radio1 == 'on') {
+                this.quantidade_pilulas = 0;
+                this.switch = true;
+              } else {
+                this.quantidade_pilulas = salvar.qntdPilula;
+                this.switch = false;
+              }
+
+              this.atualizarRemedio(id);
+          },
+        },
+      ]});
+
+    await alert.present();
   }
 
 
